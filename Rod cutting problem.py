626900f0ab47ab552235eecc0143f5a1aff4b9a4
Aditya Deshpande <aditya.deshpande1331@gmail.com>
2022-02-20 14:34:53 +0530

    ver3

---
+++ Rod cutting problem.py
@@ -0,0 +1,6 @@
+n = int(input('Enter the length of rob'))
+result =[ 0 for i in range(n)]
+for i in range(2,n):
+    for j in range(i//2+1):
+        result[i] = max(result[i],j*(i-j),j*result[i-j])
+print(result[-1])
